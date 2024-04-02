6ae6bf2d7312df2a18816fd0696251b55921a8a3
yanisa <sirinyaausah46@gmail.com>
2024-04-03 00:40:41 +0700

    update receipt ,prisma,api

--- src/app/(default)/Bookroom/receip/test.tsx
+++ src/app/(default)/Bookroom/receip/test.tsx
@@ -19,21 +19,14 @@ const Receipt = () => {
   return (
     <div className='' style={{paddingTop:'7.5rem'}}>
          {receipt.map((reservation: any) => (
-            <div key={reservation.BookID}>
+            <div key={reservation.informationGuest.FirstName}>
     <div className="max-w-lg mx-auto bg-white shadow-lg rounded-lg p-6 " >
       <div className="mb-4">
-      <h2 className="text-xl font-bold">Address</h2>
-        <div className="mt-2">
-          <p className="text-gray-700">{reservation.Hotel.Address} {reservation.Hotel.City}</p>
-          <p className="text-gray-700">{reservation.Hotel.Postcode}, {reservation.Hotel.Country}</p>
-        </div>
         <h2 className="text-xl font-bold">Customer Name & Address</h2>
         <div className="mt-2">
-          <p className="text-gray-700">Name: {reservation.informationGuest.FirstName} {reservation.informationGuest.LastName}</p>
-          <div className='flex gap-1'>
-          <p className="text-gray-700">Booking Address: {reservation.informationGuest.Address} {reservation.informationGuest.City}, {reservation.informationGuest.Country}</p>
-          </div>
-          <p className="text-gray-700">Email Address: {reservation.informationGuest.User.email}</p>
+          <p className="text-gray-700">Name: {reservation.BookID}</p>
+          <p className="text-gray-700">Billing Address: [Billing Address]</p>
+          <p className="text-gray-700">Email Address: [Email Address]</p>
         </div>
       </div>
       <div>
@@ -42,18 +35,16 @@ const Receipt = () => {
           <div>
             <p className="text-gray-700">Hotel Name</p>
             <p className="text-gray-700">Period</p>
-            <p className="text-gray-700 py-7">Room Type</p>
+            <p className="text-gray-700">Room Type</p>
             <p className="text-gray-700"># of Rms.</p>
-            <p className="text-gray-700"># Num adults</p>
-            <p className="text-gray-700"># Num children</p>
+            <p className="text-gray-700"># of Extra Beds</p>
           </div>
           <div className="text-right">
-            <p className="text-gray-700">{reservation.Hotel.Hotelname}</p>
+            <p className="text-gray-700">Yao In Chan Hotel</p>
             <p className="text-gray-700">December 6, 2014 - December 7, 2014 1 night(s)</p>
             <p className="text-gray-700">Standard</p>
-            <p className="text-gray-700 py-8">1</p>
-            <p className="text-gray-700" style={{marginTop:'-34px'}}>{reservation.Numadults}</p>
-            <p className="text-gray-700">{reservation.Numchild}</p>
+            <p className="text-gray-700">1</p>
+            <p className="text-gray-700">0</p>
           </div>
         </div>
       </div>
@@ -61,19 +52,24 @@ const Receipt = () => {
         <div className="grid grid-cols-2 gap-4">
           <div>
             <p className="text-gray-700">Total Room Charges</p>
-            <p className="text-gray-700">Total Enhancment Charges</p>
+            <p className="text-gray-700">Total Extra Bed Charges</p>
+            <p className="text-gray-700">Discount (SCB discount)</p>
           </div>
           <div className="text-right">
-            <p className="text-gray-700">THB {(reservation.Totalprice * 0.07 +reservation.Totalprice * 0.10 + reservation.Totalprice  ).toFixed(2)}</p>
-            <p className="text-gray-700">THB {reservation.Enhanc.EnhancPrice}.00</p>
+            <p className="text-gray-700">USD 76.85</p>
+            <p className="text-gray-700">USD 0.00</p>
+            <p className="text-gray-700">USD -4.57</p>
           </div>
         </div>
         <div className="mt-4 border-t border-gray-300 pt-4">
           <div className="flex justify-between">
             <p className="text-gray-700 font-bold">GRAND TOTAL</p>
-            <p className="text-gray-700 font-bold">THB {(reservation.Totalprice * 0.07 +reservation.Totalprice * 0.10 + reservation.Totalprice  + reservation.Enhanc.EnhancPrice ).toFixed(2)}</p>
+            <p className="text-gray-700 font-bold">USD 72.28</p>
           </div>
-          
+          <div className="flex justify-between mt-2">
+            <p className="text-gray-700">Total Charge to Credit Card</p>
+            <p className="text-gray-700">THB 2,343.90 (USD 72.28)</p>
+          </div>
         </div>
       </div>
     </div>
