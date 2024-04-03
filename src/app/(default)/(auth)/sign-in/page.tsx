65efabac29f062e6977ed448e5a1e9dca8c66f29
Yaya25aom <99474159+Yaya25aom@users.noreply.github.com>
2024-04-03 20:32:41 +0700

    Update page.tsx

--- src/app/(default)/(auth)/sign-in/page.tsx
+++ src/app/(default)/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from '@/components/form/SignInForm';
 
 const page = () => {
   return (
-    <div className='w-[30%]'>
+    <div className='w-full'>
       <SignInForm />
     </div>
   );
