939ff7dd05cf1f68474866a5f85c97a368b9f69c
Paradii <pirada.bt@gmail.com>
2024-04-03 10:02:54 +0700

    edit / sign in-up / width full scale to 30%

--- src/app/(default)/(auth)/sign-in/page.tsx
+++ src/app/(default)/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from '@/components/form/SignInForm';
 
 const page = () => {
   return (
-    <div className='w-full'>
+    <div className='w-[30%]'>
       <SignInForm />
     </div>
   );
--- src/app/(default)/(auth)/sign-up/page.tsx
+++ src/app/(default)/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from '@/components/form/SignUpForm';
 
 const page = () => {
   return (
-    <div className='w-full'>
+    <div className='w-[30%]'>
       <SignUpForm />
     </div>
   );
