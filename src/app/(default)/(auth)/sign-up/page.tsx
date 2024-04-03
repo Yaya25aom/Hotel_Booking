b4af037011dddd92b80fa93c26e61d571603c807
Yaya25aom <99474159+Yaya25aom@users.noreply.github.com>
2024-04-03 20:33:08 +0700

    Update page.tsx

--- src/app/(default)/(auth)/sign-up/page.tsx
+++ src/app/(default)/(auth)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from '@/components/form/SignUpForm';
 
 const page = () => {
   return (
-    <div className='w-[30%]'>
+    <div className='w-full'>
       <SignUpForm />
     </div>
   );
